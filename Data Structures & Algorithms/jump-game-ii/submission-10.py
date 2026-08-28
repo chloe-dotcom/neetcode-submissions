f38aa45e035cb84497efdb6f8b79b5de7a7fad13
chloe-dotcom <chloez@mit.edu>
2026-08-28 11:22:23 -0700

    Add: jump-game-ii - submission-10

---
+++ Data Structures & Algorithms/jump-game-ii/submission-10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        currIndex = 0
+        currJumpRange = nums[0]
+        count = 0
+
+        while currIndex < len(nums)-1:
+            temp = 0
+            if currIndex + currJumpRange >= len(nums)-1:
+                return count + 1
+            count += 1
+            for i in range(currIndex+1, currIndex+currJumpRange+1):
+                if i + nums[i] >= temp:
+                    temp = nums[i] + i
+                    currIndex = i
+            currJumpRange = nums[currIndex]
+        
+        return count
