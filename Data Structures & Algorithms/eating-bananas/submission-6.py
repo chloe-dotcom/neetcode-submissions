02dbc314b45045021d411a5a7fc78add319d5c19
chloe-dotcom <chloez@mit.edu>
2026-09-03 16:43:21 -0400

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if not piles:
+            return 0
+
+        maxK = max(piles)
+        minK = 1
+        best = maxK
+
+        while minK <= maxK:
+            current_k = int((minK + maxK)//2)
+            current_time = 0
+            for group in piles:
+                current_time += math.ceil(group / current_k)
+
+            if current_time > h:
+                minK = current_k + 1
+            
+            else:
+                best = current_k
+                maxK = current_k - 1
+        
+        return best
